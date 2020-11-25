613c7843dfad9d857c95c3dee5a643a0d0bc3af3
Joshua Meritt <36414900+M3ritt@users.noreply.github.com>
2020-11-25 16:24:14 -0500

    Update digital_signatures.py
    Added new function to get average time of entered amount bit size and of threads to be ran.

--- Demonstration/digital_signatures.py
+++ Demonstration/digital_signatures.py
@@ -16,10 +16,15 @@ import time
 from Cryptodome.PublicKey import RSA
 # sha512 is used to create a hash of the encoded sent data
 from hashlib import sha512
+# threading is used to have multiple threads to get average time it takes to run for certain bit size
+from threading import Thread
 
 # Global bit_size for bit_size for easy change when needed
 global bit_size
 bit_size = 1024
+# Global average_time for average_time will be altered a lot by threads
+global average_time
+average_time = 0
 
 
 def generate(comparing):
@@ -77,13 +82,15 @@ def verify(signature, key_pair, new_data, comparing):
     """
     valid_signature = str.encode(new_data)
     hashed = int.from_bytes(sha512(valid_signature).digest(), byteorder='big')
-    if not comparing:
-        print("\n[Hash of received data]: \n", hashed)
     hash_from_signature = pow(signature, key_pair.e, key_pair.n)
     if not comparing:
         print("\n[Hash of sent data]: \n", hash_from_signature)
+        print("\n[Hash of received data]: \n", hashed)
 
-    return hashed == hash_from_signature
+    if hashed == hash_from_signature:
+        return "[Valid]"
+    else:
+        return "[Invalid]"
 
 
 def digital_signature_runner(text, is_valid_input, comparing):
@@ -98,14 +105,9 @@ def digital_signature_runner(text, is_valid_input, comparing):
         @return:
             is_valid : boolean based on whether the signature is valid or not
     """
-    print("\n [Validating] ")
     generated_key_pair = generate(comparing)
     signature = signing(generated_key_pair, text)
     is_valid = verify(signature, generated_key_pair, is_valid_input, comparing)
-    if is_valid:
-        is_valid = "[Valid]"
-    else:
-        is_valid = "[Invalid]"
     if not comparing:
         print("\n[Signature]: \n", signature, " \n\n[Signature validity]: ", is_valid)
     return is_valid
@@ -127,6 +129,8 @@ def compare_bit_sizes(first_bit_size, second_bit_size, text, is_valid_text):
                 - second_bit_size with the time it took to run
                 - time difference between the two
     """
+    global bit_size
+    temp_bit_size = bit_size
     first_run_time, is_valid = comparing_helper(first_bit_size, text, is_valid_text)
     second_run_time, is_valid = comparing_helper(second_bit_size, text, is_valid_text)
     time_difference = abs(first_run_time - second_run_time)
@@ -134,6 +138,7 @@ def compare_bit_sizes(first_bit_size, second_bit_size, text, is_valid_text):
     print("\n [First bit size]:", first_bit_size, " [time]: ", first_run_time)
     print(" [Second bit size]:", second_bit_size, " [time]: ", second_run_time)
     print("\n [Time difference]: ", time_difference, " [Signature validity]:  ", is_valid)
+    bit_size = temp_bit_size
 
 
 def compare_three_bits(first_bit_size, second_bit_size, third_bit_size, text, is_valid_text):
@@ -154,6 +159,8 @@ def compare_three_bits(first_bit_size, second_bit_size, third_bit_size, text, is
                 - third_bit_size with the time it took to run
                 - time difference between the two
     """
+    global bit_size
+    temp_bit_size = bit_size
     first_run_time, is_valid = comparing_helper(first_bit_size, text, is_valid_text)
     second_run_time, is_valid = comparing_helper(second_bit_size, text, is_valid_text)
     third_run_time, is_valid = comparing_helper(third_bit_size, text, is_valid_text)
@@ -162,6 +169,7 @@ def compare_three_bits(first_bit_size, second_bit_size, third_bit_size, text, is
     print(" [Second bit size]:", second_bit_size, " [time]: ", second_run_time)
     print(" [Third bit size]:", third_bit_size, " [time]: ", third_run_time)
     print("\n [Signature validity]:  ", is_valid)
+    bit_size = temp_bit_size
 
 
 def comparing_helper(current_bit_size, text, is_valid_text):
@@ -182,11 +190,85 @@ def comparing_helper(current_bit_size, text, is_valid_text):
     """
     global bit_size
     bit_size = int(current_bit_size)
+    print("\n [Validating] ")
     start = time.time()
     is_valid = digital_signature_runner(text, is_valid_text, True)
     end = time.time()
     run_time = (end - start)
     return run_time, is_valid
+
+
+def find_average_time(text, is_valid_text):
+    """
+        Helper method to find average time of certain bit size/byte size of data.
+            - Is method call from threading --> updates global variable instead of returning anything
+
+        @param:
+            text : The data that was sent
+            is_valid_text : The data that was received
+    """
+    global average_time
+    start_time = time.time()
+    digital_signature_runner(text, is_valid_text, True)
+    end_time = time.time()
+    average = (end_time - start_time)
+    average_time += average
+
+
+def threading(text, is_valid_text, num):
+    """
+        Main method to find average time. Uses Threads to find average time based on:
+            - bit size of key pair
+            -byte size of data being 'sent'
+
+        Note:
+            Since threads are being created and run, the time for each thread may slow down due to computer not being
+            strong enough but is still more time efficient, time running wise, to use threads instead of loop
+
+        @param:
+            text : The data that was sent
+            is_valid_text : The data that was received
+            num : the number of threads to be created
+
+        @print:
+            - The average time
+            - Sent/Received data size in bytes
+            - Number of threads created
+    """
+    global average_time
+    average_time = 0
+    threads = []
+    for n in range(num):
+        print("[Validating]\n")
+        thread = Thread(target=find_average_time, args=(text, is_valid_text))
+        thread.start()
+        threads.append(thread)
+
+    for t in threads:
+        t.join()
+
+    average_time /= num
+    sent_byte_size = len(text.encode('utf-8'))
+    received_byte_size = len(is_valid_text.encode('utf-8'))
+    print("[Average time] : ", average_time, ' [Sent data size] :', sent_byte_size, ' bytes  [Received data size] : ',
+          received_byte_size, ' bytes [Runs] : ', num)
+
+
+def number_checker():
+    """
+        Method to error check the number of runs to ensure that it is a number.
+
+        @return:
+            value :  The value of the number
+    """
+    while True:
+        num = input("\n@User: Number of runs to average: \n")
+        try:
+            value = int(num)
+            print("\n")
+            return value
+        except ValueError or TypeError:
+            print("[Error] Incorrect type. Try an integer.")
 
 
 def bit_size_checking(some_bit_size):
@@ -243,19 +325,21 @@ def commands():
 
                   "--compare_three_bit_sizes : "
                   "can compare three different bit sizes and time to create and validate signatures for some data \n"
-
+                
+                  "--average_time : finds the average time to run based on given number of trials\n"
+                  
                   "--exit : exits the program")
 
         elif command == "--run":
             text = input("@User: Data to be sent: \n")
             is_valid_input = input("\n@User: Data that is received: \n")
+            print("\n [Validating] ")
             digital_signature_runner(text, is_valid_input, False)
 
         elif command == "--change_bit_size":
             print("\n")
-            new_bit_size = get_new_bit_size()
             global bit_size
-            bit_size = int(new_bit_size)
+            bit_size = int(get_new_bit_size())
             print("\n [Success] The bit size was changed to: ", bit_size)
 
         elif command == "--compare_bit_size":
@@ -282,6 +366,13 @@ def commands():
             text = input("\n@User: What is the sent data?\n")
             is_valid_text = input("\n@User: What is the received data?\n")
             compare_three_bits(first_bit_size, second_bit_size, third_bit_size, text, is_valid_text)
+
+        elif command == "--average_time":
+            text = input("@User: Data to be sent: \n")
+            is_valid_input = input("\n@User: Data that is received: \n")
+            num = number_checker()
+            bit_size = int(get_new_bit_size())
+            threading(text, is_valid_input, num)
 
         elif command == "--exit":
             print("\n [Exiting]")
